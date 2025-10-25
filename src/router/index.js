af456b038f875a8dcae11655a85e5bb36ecab795
aabh0002 <aabh0002@student.monash.edu>
2025-10-25 13:03:16 +1100

    changed base_url

--- src/router/index.js
+++ src/router/index.js
@@ -54,7 +54,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory("https://vuelibrary.pages.dev"),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
